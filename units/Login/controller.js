35336db1f31b42a34bd1efded609f6b2e340fa1e
Manu Perera <manu@wso2.com>
2014-06-23 16:12:26 +0530

    adding edit input feature.

--- units/Login/controller.js
+++ units/Login/controller.js
@@ -1,1 +1,7 @@
-page.userName = 'bla';
+var userName = request.getParameter('userName');
+var password = request.getParameter('password');
+if(userName=='manu' && password=='manu'){
+    new Log().info('****' + page.isLogged);
+    page.isLogged = true;
+    new Log().info('****' + page.isLogged);
+}
